a910190f727366e1eec46fb8f5d1a7fd6a541c6e
Cristóbal Sarmiento Rodríguez <cristobal.sarmiento101@alu.ulpgc.es>
2024-01-01 18:02:05 +0000

    Class Main created

---
+++ src/software/ulpgc/kata1/Main.java
@@ -0,0 +1,14 @@
+package software.ulpgc.kata1;
+
+import java.time.LocalDate;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Person cristobal = new Person("Cristobal", LocalDate.of(2003, 3, 3));
+        System.out.println(cristobal);
+
+    }
+
+}
